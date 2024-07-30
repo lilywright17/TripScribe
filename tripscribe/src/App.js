a4fbb0c17052b89d51cfe3c20e2e296f58747ecd
Lily Wright <lilywright93@gmail.com>
2024-07-30 19:06:23 +0100

    Created react app and added template for everyone to fill in

---
+++ tripscribe/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          
+        <h1>Welcome to our Travel App!</h1>
+      </header>
+        <h2>Our Favourite Trips</h2>
+        <ul>
+          <li>
+            <h2>Precious</h2>
+            <p>FILL IN TRIP DETAILS HERE</p>
+          </li>
+          <li>
+            <h2>Krystal</h2>
+              <p>FILL IN TRIP DETAILS HERE</p>
+            </li>
+            <li>
+              <h2>Mediha</h2>
+              <p>FILL IN TRIP DETAILS HERE</p>
+            </li>
+            <li>
+              <h2>Victoria</h2>
+              <p>FILL IN TRIP DETAILS HERE</p>
+            </li>
+            <li>
+              <h2>Marta</h2>
+              <p>FILL IN TRIP DETAILS HERE</p>
+            </li>
+            <li>
+              <h2>Katie</h2>
+              <p>FILL IN TRIP DETAILS HERE</p>
+            </li>
+            <li>
+              <h2>Lily</h2>
+              <p>FILL IN TRIP DETAILS HERE</p>
+            </li>
+        </ul>
+  
+  
+      </div>
+    );
+  }
+
+export default App;

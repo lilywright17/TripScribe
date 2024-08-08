c7f7c56c91289be8fe56b0157665a89761b73bf1
atepem <precious.joveres@gmail.com>
2024-08-08 14:05:44 +0100

    tripscribe db creation table added

---
+++ backend/tripscribedb.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE tripscribeDB;
+USE tripscribeDB;
+
+-- Users table
+CREATE TABLE Users (
+	userID INT AUTO_INCREMENT PRIMARY KEY,
+    fullname VARCHAR(70),
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    pword_hash VARCHAR(255) NOT NULL
+);
+
+-- Trips table
+CREATE TABLE Trips (
+	tripID INT AUTO_INCREMENT primary KEY,
+    userID INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    date_from DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    date_to DATETIME,
+    FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
+-- Location table
+CREATE TABLE Locations (
+	locationID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    latitude DECIMAL(10,8) NOT NULL,
+    longitude DECIMAL(11,8) NOT NULL
+);
+
+-- Photos table
+CREATE TABLE Photos (
+	photoID INT AUTO_INCREMENT PRIMARY KEY,
+    tripID INT NOT NULL,
+    photo_url VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    FOREIGN KEY (tripID) REFERENCES Trips(tripID)
+);
+
+-- Trips-Location joining table
+CREATE TABLE Trip_Location (
+	tripID INT NOT NULL,
+    locationID INT NOT NULL,
+    FOREIGN KEY (tripID) REFERENCES Trips(tripID),
+    FOREIGN KEY (locationID) REFERENCES Locations(locationID)
+);
+    

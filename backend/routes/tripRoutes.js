a85d289ecf158b4b10729a13a47ae1bb250c8453
Mediha HM <mediha.junkhm@gmail.com>
2024-08-13 15:11:07 +0100

    Added backend structure files, added register api logic and fixed login backend logic

---
+++ backend/routes/tripRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { getTrips } = require('../controllers/tripController');
+
+const router = express.Router();
+
+// All the trip related routes will be included below and the imported in server.js
+router.post('/trips', getTrips);
+
+module.exports = router;

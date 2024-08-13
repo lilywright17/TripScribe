2c3be60430084503b9391ea683bf556085c1f17e
Krystal Lake <krystal.lake10@gmail.com>
2024-08-13 09:47:33 +0100

    fixed formatting in register

--- tripscribe/src/pages/register/register.jsx
+++ tripscribe/src/pages/register/register.jsx
@@ -120,4 +120,45 @@ export const Register = () => {
 
             <div className={`login-box`}>
                 <div className='login-container'>
-                    {
+                    {/* <h1>Sign In</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                autoFocus
+                                type="email"
+                                id="email"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <div className='button-container'>
+                            <button
+                                type="submit"
+                                disabled={!validateForm()}
+                            >
+                                LOG IN
+                            </button>
+                        </div>
+                    </form>
+                    <div className='button-container'>
+                        <button onClick={toggleForm}>REGISTER</button>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    );
+}

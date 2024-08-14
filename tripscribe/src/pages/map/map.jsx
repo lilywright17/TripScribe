18d621bf20e509bea9be9f61d95533a30714c87d
Lily Wright <lilywright93@gmail.com>
2024-08-14 13:13:40 +0100

    moved API key to .env file

--- tripscribe/src/pages/map/map.jsx
+++ tripscribe/src/pages/map/map.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import './map.css';
-
+import { tripsArray } from './tripsArray.js'
 import {
   APIProvider,
   Map,
+  Marker
 } from "@vis.gl/react-google-maps";
+
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const MapPage = () => (
   <APIProvider
-    apiKey={"AIzaSyBXeEY40DPUFtPT41BSxfbpn20T_xKQvGY"}
+    apiKey={apiKey}
     onLoad={() => console.log("Maps API has loaded.")}
   >
     <div className = "map-div">
@@ -23,7 +26,11 @@ export const MapPage = () => (
             ev.detail.zoom
           )
         }
-      ></Map>
+      >{/* Render a Marker for each location */}
+      {tripsArray.map((trip) => (
+  <Marker key={trip.tripID} position={{ lat: trip.latitude, lng: trip.longitude }} />
+))}</Map>
+
     </div>
   </APIProvider>
 );

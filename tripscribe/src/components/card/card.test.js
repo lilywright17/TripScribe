c28cc2ae2131f1eb800a78c5c4024ed38887d7ae
Katie-W-22 <katiecoder22@gmail.com>
2024-08-14 11:52:28 +0100

    added testing of card component

---
+++ tripscribe/src/components/card/card.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+import renderer from 'react-test-renderer';
+
+test('renders card component correctly with country, city passed by props', () => {
+
+    const tree = renderer.create(<Card country="Spain" city="Madrid" />).toJSON();
+    expect(tree).toMatchSnapshot();
+}
+);
+
+// test to see if image is rendered correctly
+test('renders card component correctly with image passed by props', () => {
+    const tree = renderer.create(<Card image="https://www.example.com/image.jpg" />).toJSON();
+    expect(tree).toMatchSnapshot();
+}
+);

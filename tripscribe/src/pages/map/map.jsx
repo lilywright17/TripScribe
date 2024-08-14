d8738e8088166ff5f116efc55eaa630507d0d4df
Lily Wright <lilywright93@gmail.com>
2024-08-14 15:29:19 +0100

    moved Google API to backend .env file - all seems to work with no errors now!

--- tripscribe/src/pages/map/map.jsx
+++ tripscribe/src/pages/map/map.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import './map.css';
 import { tripsArray } from './tripsArray.js'
 import {
   APIProvider,
   Map,
-  Marker
+  Marker,
+  InfoWindow
 } from "@vis.gl/react-google-maps";
 
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
-export const MapPage = () => (
+export const MapPage = () => {
+
+   // State to keep track of the currently active marker
+   const [activeMarker, setActiveMarker] = useState(null);
+
+   // Function to handle marker click
+   const handleMarkerClick = (trip) => {
+     setActiveMarker(trip);
+    console.log(activeMarker)
+   };
+
+   return (
+
   <APIProvider
     apiKey={apiKey}
     onLoad={() => console.log("Maps API has loaded.")}
@@ -28,9 +41,19 @@ export const MapPage = () => (
         }
       >{/* Render a Marker for each location */}
       {tripsArray.map((trip) => (
-  <Marker key={trip.tripID} position={{ lat: trip.latitude, lng: trip.longitude }} />
+  <Marker key={trip.tripID} position={{ lat: trip.latitude, lng: trip.longitude }} onClick={() => handleMarkerClick(trip)}>
+    {activeMarker === trip && (
+    <InfoWindow>
+      <div>
+        <h2>{trip.title}</h2>
+        <p>{trip.description}</p>
+        <p>{trip.startDate} to {trip.endDate}</p>
+      </div>
+    </InfoWindow>
+  )}
+  </Marker>
 ))}</Map>
 
     </div>
   </APIProvider>
-);
+)};

01e0a9c7056d78ae753429f4ca29952d04f6cb82
Lily Wright <lilywright93@gmail.com>
2024-08-20 11:41:03 +0100

    changes to mockdata.sql

--- backend/mockData.sql
+++ backend/mockData.sql
@@ -1,10 +1,13 @@
+USE tripscribeDB;
+
 -- SQL script with mock data for the Users and Trips table
 INSERT INTO Users (userID, fullname, username, email, pword_hash)
 VALUES 
 (1, 'Ada Lovelace', 'adalovelace', 'ada.lovelace@gmail.com', '$2b$10$A/FfTkuxVxuuzobymwpNCOzDLsctzpX3HN0KSmlOFKgjmF5YoLp9W'),
 (2, 'Rosalind Franklin', 'rosalindf', 'rosalind.franklin@gmail.com', '$2b$10$6sub3KkksQrBdy2VjpBTF.qK6F8XzsEhpqxXCV4kno.3s0L1To02W'),
 (3, 'Lise Meitner', 'lisemeitner', 'lise.meitner@gmail.com', '$2y$10$T9zXnRZb.xS4G6HGi21O6ujm8IaUM5K4/1T/QF2bqW1aUIRbMqTAJ'),
-(4,	'Victoria Test', 'victoriatest01', 'th4les.vic.jkm@gmail.com', '$2b$10$URSNfNyaCimQwnNssm65MuXDeNbyd74XoG4HW6sqN3SIC9VIeOrQC');
+(4,	'Victoria Test', 'victoriatest01', 'th4les.vic.jkm@gmail.com', '$2b$10$URSNfNyaCimQwnNssm65MuXDeNbyd74XoG4HW6sqN3SIC9VIeOrQC'),
+(5, 'Lily', 'lily93', 'lilywright93@gmail.com', '$2b$10$Qw859hFvbPC0fUCKeYWxFu/giGrgJIItxZAXX.GMzzOAbZgrd5oYq');
 
 INSERT INTO Trips (tripID, userID, city, country, description, date_from, date_to)
 VALUES 
@@ -33,24 +36,35 @@ VALUES
 (14, 4, 'Bilbao', 'Spain', 'Exploring the rich Basque culture, visiting the iconic Guggenheim Museum, and enjoying pintxos in the old town.', '2024-08-01 00:00:00', '2024-09-07 00:00:00'),
 (15, 4, 'Vancouver', 'Canada', 'Exploring the breathtaking landscapes of the Pacific Northwest, with hikes in the mountains and visits to vibrant local markets.', '2024-08-16 00:00:00', '2024-09-01 00:00:00');
 
-INSERT INTO Photos (photoID, tripID, userID, secure_url, alt_text )
-VALUES 
--- Photos for userID=4 // tripID= 10, 11, 13, ,14
-(1, 10, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723898930/hsp9umyioxxlkvcnmmux.png', NULL),
-(2, 10, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723898933/cwqdgzlvml95zmntkqzj.png', NULL),
-(3, 10, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723898932/kbg7xrtiwtybtensfmnk.png', NULL),
-(4, 11, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899190/s6eh9lm9mp3pom1lhgpo.png', NULL),
-(5, 11, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899189/j92deqzionxkvu2ljbzp.png', NULL),
-(6, 11, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899189/slmrxyyvmja0lpxc4voi.png', NULL),
-(7, 12, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899279/r4zcxz1c2redpqcqdrq6.png', NULL),
-(8, 12, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899279/e62fvnrw1eeeztywbyv6.png', NULL),
-(9, 12, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899280/d45qtocfl7xs9i2xwylu.png', NULL),
-(10, 13, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899690/pug27wg2x2jqq9xxcv9z.png', NULL),
-(11, 13, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899689/iogt29g9h5hjwozbtuag.png', NULL),
-(12, 13, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899688/epmazjwpazugaixmi7yz.png', NULL),
-(13, 14, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723900899/jrgaoyckwtniuemthtgo.png', NULL),
-(14, 14, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723900899/pezhjnx4azgrolhhjdvo.png', NULL),
-(15, 14, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723900905/nwikftoiyd8fqudgr22u.png', NULL);
+-- Trips for Lily testing map page
+INSERT INTO Trips (tripID, userID, city, country, description, date_from, date_to)
+VALUES
+    (16, 5, 'Marrakech', 'Morocco', 'Exploring the vibrant souks, tasting exotic spices, and experiencing the rich history of this ancient city.', '2024-09-10 00:00:00', '2024-09-20 00:00:00'),
+    (17, 5, 'Cairo', 'Egypt', 'Standing in awe of the Pyramids of Giza, cruising the Nile, and discovering ancient Egyptian history.', '2024-10-01 00:00:00', '2024-10-15 00:00:00'),
+    (18, 5, 'Kyoto', 'Japan', 'Wandering through serene temples, admiring the beauty of cherry blossoms, and soaking in traditional hot springs.', '2024-11-05 00:00:00', '2024-11-20 00:00:00'),
+    (19, 5, 'Lisbon', 'Portugal', 'Enjoying the picturesque views from Alfama, tasting delicious pastel de nata, and listening to fado music.', '2024-12-01 00:00:00', '2024-12-10 00:00:00'),
+    (23, 5, 'Cape Town', 'South Africa', 'Experiencing the stunning views from Table Mountain, exploring vibrant neighborhoods, and enjoying local wines.', '2024-11-25 00:00:00', '2024-12-05 00:00:00'),
+    (24, 5, 'Queenstown', 'New Zealand', 'Thrilling adventure sports in the adventure capital of the world, surrounded by breathtaking landscapes.', '2024-12-15 00:00:00', '2024-12-30 00:00:00');
+
+
+-- INSERT INTO Photos (photoID, tripID, userID, secure_url, alt_text )
+-- VALUES 
+-- -- Photos for userID=4 // tripID= 10, 11, 13, ,14
+-- (1, 10, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723898930/hsp9umyioxxlkvcnmmux.png', NULL),
+-- (2, 10, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723898933/cwqdgzlvml95zmntkqzj.png', NULL),
+-- (3, 10, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723898932/kbg7xrtiwtybtensfmnk.png', NULL),
+-- (4, 11, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899190/s6eh9lm9mp3pom1lhgpo.png', NULL),
+-- (5, 11, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899189/j92deqzionxkvu2ljbzp.png', NULL),
+-- (6, 11, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899189/slmrxyyvmja0lpxc4voi.png', NULL),
+-- (7, 12, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899279/r4zcxz1c2redpqcqdrq6.png', NULL),
+-- (8, 12, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899279/e62fvnrw1eeeztywbyv6.png', NULL),
+-- (9, 12, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899280/d45qtocfl7xs9i2xwylu.png', NULL),
+-- (10, 13, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899690/pug27wg2x2jqq9xxcv9z.png', NULL),
+-- (11, 13, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899689/iogt29g9h5hjwozbtuag.png', NULL),
+-- (12, 13, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723899688/epmazjwpazugaixmi7yz.png', NULL),
+-- (13, 14, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723900899/jrgaoyckwtniuemthtgo.png', NULL),
+-- (14, 14, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723900899/pezhjnx4azgrolhhjdvo.png', NULL),
+-- (15, 14, 4, 'https://res.cloudinary.com/cfgvicteam5/image/upload/v1723900905/nwikftoiyd8fqudgr22u.png', NULL);
 
 INSERT INTO Photos (photoID, tripID, userID, secure_url, alt_text)
 VALUES 
@@ -76,3 +90,67 @@ SELECT * FROM Photos;
 -- select * from Users;
 -- SET SQL_SAFE_UPDATES = 0;
 -- DELETE FROM Users WHERE username='Jsmith';
+
+-- Adding in latitude and longitude for maps page
+INSERT INTO Locations (name, latitude, longitude)
+VALUES
+('Lisbon', 38.716891, -9.139883),
+('Madrid', 40.416775, -3.703790),
+('Berlin', 52.520008, 13.404954),
+('Rome', 41.902782, 12.496366),
+('Cambridge', 52.205337, 0.121817),
+('Istanbul', 41.008240, 28.978359),
+('New York', 40.712776, -74.005974),
+('Zurich', 47.376887, 8.541694),
+('Stockholm', 59.329323, 18.068581),
+('Vienna', 48.210033, 16.363449),
+('Tokyo', 35.689487, 139.691711),
+('Reykjavik', 64.135483, -21.895414),
+('Sydney', -33.868820, 151.209290),
+('Rio de Janeiro', -22.906847, -43.172897),
+('Bilbao', 43.263012, -2.934985),
+('Vancouver', 49.282729, -123.120738),
+('Marrakech', 31.629472, -7.981084),
+('Cairo', 30.044420, 31.235712),
+('Kyoto', 35.011636, 135.768029),
+('Cape Town', -33.924870, 18.424055),
+('Queenstown', -45.031162, 168.662643);
+
+INSERT INTO Trip_Location (tripID, locationID)
+VALUES
+-- For Ada Lovelace
+(1, (SELECT locationID FROM Locations WHERE name = 'Lisbon')),
+(2, (SELECT locationID FROM Locations WHERE name = 'Madrid')),
+(3, (SELECT locationID FROM Locations WHERE name = 'Berlin')),
+(4, (SELECT locationID FROM Locations WHERE name = 'Rome')),
+(5, (SELECT locationID FROM Locations WHERE name = 'Cambridge')),
+(6, (SELECT locationID FROM Locations WHERE name = 'Istanbul')),
+
+-- For Rosalind Franklin
+(20, (SELECT locationID FROM Locations WHERE name = 'New York')),
+(21, (SELECT locationID FROM Locations WHERE name = 'Cambridge')),
+(22, (SELECT locationID FROM Locations WHERE name = 'Zurich')),
+
+-- For Lise Meitner
+(7, (SELECT locationID FROM Locations WHERE name = 'Berlin')),
+(8, (SELECT locationID FROM Locations WHERE name = 'Stockholm')),
+(9, (SELECT locationID FROM Locations WHERE name = 'Vienna')),
+
+
+-- For Victoria Test
+(10, (SELECT locationID FROM Locations WHERE name = 'Tokyo')),
+(11, (SELECT locationID FROM Locations WHERE name = 'Reykjavik')),
+(12, (SELECT locationID FROM Locations WHERE name = 'Sydney')),
+(13, (SELECT locationID FROM Locations WHERE name = 'Rio de Janeiro')),
+(14, (SELECT locationID FROM Locations WHERE name = 'Bilbao')),
+(15, (SELECT locationID FROM Locations WHERE name = 'Vancouver')),
+
+-- For Lily's test
+    (16, (SELECT locationID FROM Locations WHERE name = 'Marrakech')), 
+    (17, (SELECT locationID FROM Locations WHERE name = 'Cairo')), 
+    (18, (SELECT locationID FROM Locations WHERE name = 'Kyoto')), 
+    (19, (SELECT locationID FROM Locations WHERE name = 'Lisbon')), 
+    (23, (SELECT locationID FROM Locations WHERE name = 'Cape Town')),
+    (24, (SELECT locationID FROM Locations WHERE name = 'Queenstown'));
+    
+    
